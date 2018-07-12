2f325e1a30d9357bcd7eef0e8c0e96d9d5b56d4c
Zamarise Montoya <zamarise@gmail.com>
2018-07-11 20:06:59 -0700

    Initial project files

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const profile = require('./profile');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/profile', profile);
+
+// Here we're setting the views directory to be ./views
+// this lets the app know where to find the template files
+app.set('views', './views');
+
+// Here we're setting the default engine to be ejs
+// note we don't need to require it; express will do that for us.
+app.set('view engine', 'ejs');
+
+// Now instead of using res.send we can use
+// res.render to send the output of the template by filename
+app.get('/', (req, res) => {
+  const data = {
+    person: {
+      firstName: 'Zam',
+      lastName: 'Montoya'
+    }
+  };
+
+  // Notice now the data is the second argument passed to the template render method
+  res.render('index', data);
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.post('/thanks', (req, res) => {
+  res.render('thanks', { contact: req.body });
+});
+
+app.listen(8080, () => {
+  console.log('listening at http://localhost:8080');
+});
